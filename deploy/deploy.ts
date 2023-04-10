f6012f388ddacb7a344a33954ecb0200b61cf122
Akira-Taniguchi <dededededaiou2003@yahoo.co.jp>
2023-04-10 14:23:03 +0900

    test

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -1,69 +1,55 @@
-/* eslint-disable @typescript-eslint/prefer-readonly-parameter-types */
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable eqeqeq */
-/* eslint-disable @typescript-eslint/restrict-template-expressions */
-import { /* utils, */ Wallet } from 'zksync-web3'
-// Import * as ethers from 'ethers'
-import type { HardhatRuntimeEnvironment } from 'hardhat/types'
-import { Deployer } from '@matterlabs/hardhat-zksync-deploy'
-import * as dotenv from 'dotenv'
+import { utils, Wallet } from "zksync-web3";
+import * as ethers from "ethers";
+import type { HardhatRuntimeEnvironment } from "hardhat/types";
+import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
 
 // An example of a deploy script that will deploy and call a simple contract.
 export default async function (hre: HardhatRuntimeEnvironment) {
-	console.log(`Running deploy script for the Greeter contract`)
-	dotenv.config()
-	console.log(`private key:${process.env.PRIVATE_KEY}`)
-	// Initialize the wallet.
-	const wallet = new Wallet(process.env.PRIVATE_KEY!)
-	const { address } = wallet
-	console.log(`address:${address.toString()}`)
+  console.log(`Running deploy script for the Greeter contract`);
 
-	// Create deployer object and load the artifact of the contract we want to deploy.
-	const deployer = new Deployer(hre, wallet)
-	const artifact = await deployer.loadArtifact('Greeter')
+  // Initialize the wallet.
+  const wallet = new Wallet("<WALLET-PRIVATE-KEY>");
 
-	// // Deposit some funds to L2 in order to be able to perform L2 transactions.
-	// const depositAmount = ethers.utils.parseEther('0.001')
-	// const depositHandle = await deployer.zkWallet.deposit({
-	// 	to: deployer.zkWallet.address,
-	// 	token: utils.ETH_ADDRESS,
-	// 	amount: depositAmount,
-	// })
-	// // Wait until the deposit is processed on zkSync
-	// await depositHandle.wait()
+  // Create deployer object and load the artifact of the contract we want to deploy.
+  const deployer = new Deployer(hre, wallet);
+  const artifact = await deployer.loadArtifact("Greeter");
 
-	// Deploy this contract. The returned object will be of a `Contract` type, similarly to ones in `ethers`.
-	// `greeting` is an argument for contract constructor.
-	const greeting = 'Hi there!'
-	const greeterContract = await deployer.deploy(artifact, [greeting])
-	// Show the contract info.
-	const contractAddress = greeterContract.address
-	console.log(`${artifact.contractName} was deployed to ${contractAddress}`)
+  // Deposit some funds to L2 in order to be able to perform L2 transactions.
+  const depositAmount = ethers.utils.parseEther("0.001");
+  const depositHandle = await deployer.zkWallet.deposit({
+    to: deployer.zkWallet.address,
+    token: utils.ETH_ADDRESS,
+    amount: depositAmount,
+  });
+  // Wait until the deposit is processed on zkSync
+  await depositHandle.wait();
 
-	// Call the deployed contract.
-	const greetingFromContract = await greeterContract.greet()
-	if (greetingFromContract == greeting) {
-		console.log(`Contract greets us with ${greeting}!`)
-	} else {
-		console.error(`Contract said something unexpected: ${greetingFromContract}`)
-	}
+  // Deploy this contract. The returned object will be of a `Contract` type, similarly to ones in `ethers`.
+  // `greeting` is an argument for contract constructor.
+  const greeting = "Hi there!";
+  const greeterContract = await deployer.deploy(artifact, [greeting]);
 
-	// Edit the greeting of the contract
-	const newGreeting = 'Hey guys'
-	const setNewGreetingHandle = await greeterContract.setGreeting(newGreeting)
-	await setNewGreetingHandle.wait()
+  // Show the contract info.
+  const contractAddress = greeterContract.address;
+  console.log(`${artifact.contractName} was deployed to ${contractAddress}`);
 
-	const newGreetingFromContract = await greeterContract.greet()
-	if (newGreetingFromContract == newGreeting) {
-		console.log(`Contract greets us with ${newGreeting}!`)
-	} else {
-		console.error(
-			`Contract said something unexpected: ${newGreetingFromContract}`
-		)
-	}
+  // Call the deployed contract.
+  const greetingFromContract = await greeterContract.greet();
+  if (greetingFromContract == greeting) {
+    console.log(`Contract greets us with ${greeting}!`);
+  } else {
+    console.error(`Contract said something unexpected: ${greetingFromContract}`);
+  }
+
+  // Edit the greeting of the contract
+  const newGreeting = "Hey guys";
+  const setNewGreetingHandle = await greeterContract.setGreeting(newGreeting);
+  await setNewGreetingHandle.wait();
+
+  const newGreetingFromContract = await greeterContract.greet();
+  if (newGreetingFromContract == newGreeting) {
+    console.log(`Contract greets us with ${newGreeting}!`);
+  } else {
+    console.error(`Contract said something unexpected: ${newGreetingFromContract}`);
+  }
 }
-
-// https://v2-docs.zksync.io/api/hardhat/getting-started.html#initializing-the-project
-// contract address 0x76C56252A7A9039D8A8d4836B0f383010a3Df0A5
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,44 +1,21 @@
-import '@nomicfoundation/hardhat-toolbox'
-import '@openzeppelin/hardhat-upgrades'
-import '@nomicfoundation/hardhat-chai-matchers'
-import '@matterlabs/hardhat-zksync-deploy'
-import '@matterlabs/hardhat-zksync-solc'
-import * as dotenv from 'dotenv'
+import "@matterlabs/hardhat-zksync-deploy";
+import "@matterlabs/hardhat-zksync-solc";
 
-dotenv.config()
-
-const config = {
+module.exports = {
 	zksolc: {
-		version: '1.1.5',
-		compilerSource: 'docker',
-		settings: {
-			optimizer: {
-				enabled: true,
-			},
-			experimental: {
-				dockerImage: 'matterlabs/zksolc',
-				tag: 'v1.1.5',
-			},
-		},
+		version: '1.3.5',
+		compilerSource: "binary",
+		settings: {},
 	},
-	zkSyncDeploy: {
-		zkSyncNetwork: 'https://zksync2-testnet.zksync.dev',
-		ethNetwork: 'goerli', // Can also be the RPC URL of the network (e.g. `https://goerli.infura.io/v3/<API_KEY>`)
-	},
-	solidity: {
-		version: '0.8.16',
-		settings: {
-			optimizer: {
-				enabled: true,
-				runs: 200,
-			},
-		},
-	},
+	defaultNetwork: "zkTestnet",
 	networks: {
-		hardhat: {
-			zksync: true,
+		zkTestnet: {
+		  url: "https://testnet.era.zksync.dev", // URL of the zkSync network RPC
+		  ethNetwork: "goerli", // Can also be the RPC URL of the Ethereum network (e.g. `https://goerli.infura.io/v3/<API_KEY>`)
+		  zksync: true,
 		},
+	  },
+	solidity: {
+		version: '0.8.17',
 	},
 }
-
-export default config
